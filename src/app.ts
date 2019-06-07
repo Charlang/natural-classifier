33c4fc49f7e33fb2c02a42bb170b3fe4b7fd0520
Charlie.Lang <chaolunlang@163.com>
2019-06-07 13:01:22 +0800

    :bug: fix path not exist issue

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import natural from 'natural';
 import morgan from 'morgan';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { RequestOptions }  from 'apollo-datasource-rest';
+import fs from 'fs';
+const fsPromises = fs.promises;
 
 const { HOST = 'localhost', PORT = '3000' } = process.env;
 
@@ -31,10 +32,13 @@ export const server = new ApolloServer({
   app.use('/health-check', (_, res) =>{
       res.set('Content-Type', 'text/plain');
       res.status(200).send('ok');
-  })
+  });
 
   server.applyMiddleware({ app });
 
+  if (!fs.existsSync(`${process.cwd()}/data`)) {
+      await fsPromises.mkdir(`${process.cwd()}/data`);
+  }
   await app.listen({ host: HOST, port: PORT });
   console.log(`> ✅ 🎊 🎉 🍻 Server is ready at http://${HOST}:${PORT}${server.graphqlPath}`);
 })();
