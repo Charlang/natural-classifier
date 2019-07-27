abcf1c625a7a88fc6a593c8c2f8df6387bb9093a
Charlie.Lang <chaolunlang@163.com>
2019-07-27 16:23:16 +0800

    :art: code clean up

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,6 @@ export const server = new ApolloServer({
     introspection: true,
     playground: true,
 });
-import { insert, deleteNode, INode, ITree, NIL, maximum, minimum, search } from './redBlackTree';
 
 (async () => {
   const app = express();
@@ -43,30 +42,4 @@ import { insert, deleteNode, INode, ITree, NIL, maximum, minimum, search } from 
   }
   await app.listen({ host: HOST, port: PORT });
   console.log(`> ✅ 🎊 🎉 🍻 Server is ready at http://${HOST}:${PORT}${server.graphqlPath}`);
-
-  const tree: ITree = {
-    root: {
-      parent: NIL,
-      color: 'BLACK',
-      left: NIL,
-      right: NIL,
-      key: Number.MAX_SAFE_INTEGER,
-    },
-  };
-
-  console.error(new Date());
-  for (let i = 10000000; i > 0 ; i --) {
-    insert(tree, { key: i, parent: NIL, color: 'RED', left: NIL, right: NIL });
-  }
-  console.error(tree);
-  console.error(new Date());
-  console.error(`Maximum: ${JSON.stringify(maximum(tree.root).key)}`);
-  console.error(`Minimum: ${JSON.stringify(minimum(tree.root).key)}`);
-  console.error(new Date());
-  console.error(`Find: ${search(tree, 1024).key}`);
-  console.error(`Find: ${search(tree, 30000).key}`);
-  console.error(`Find: ${search(tree, 5000000).key}`);
-  console.error(`Find: ${search(tree, 2).key}`);
-  console.error(`Delete: ${deleteNode(tree, search(tree, 2))}`);
-  console.error(new Date());
 })();
